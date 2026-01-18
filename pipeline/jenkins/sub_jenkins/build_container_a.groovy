5ba66b887b8d744309b3e51dc22e4dd06f933848
18naive <18naive@gmail.com>
2026-01-18 19:56:36 +0800

    test

--- pipeline/jenkins/sub_jenkins/build_container_a.groovy
+++ pipeline/jenkins/sub_jenkins/build_container_a.groovy
@@ -1,12 +1,7 @@
 def call() {
-    def tool_path = pwd()
-    echo "Current workspace: $tool_path"
-
-    echo "test_var: $test_var"
     def ci_tool_path = pwd() + "/pipeline/ci_tool"
-    echo "ci_tool_path: $ci_tool_path"
 
-    stage("compiler") {
+    stage("Compiler") {
         echo "Start compiler ..."
         sh "python3 $ci_tool_path/binary_compare.py"
     }
