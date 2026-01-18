50a1f61caf3d0a0a797b41f4956f33f35f59e5fb
18naive <18naive@gmail.com>
2026-01-18 18:58:39 +0800

    bug fix

--- pipeline/build/build_container_a.groovy
+++ pipeline/build/build_container_a.groovy
@@ -3,8 +3,7 @@ def call() {
     echo "Current workspace: $tool_path"
 
     echo "test_var: $test_var"
-    def ci_tool_dir = "pipeline/ci_tool"
-    def ci_tool_path = pwd() + ci_tool_dir
+    def ci_tool_path = pwd() + "/pipeline/ci_tool"
     echo "ci_tool_path: $ci_tool_path"
 
     stage("compiler") {
