ef3558e26533ccd4e574ce119a07030786cf4992
18naive <18naive@gmail.com>
2026-01-21 01:05:36 +0800

    test

--- pipeline/jenkins/sub_jenkins/build_container_a.groovy
+++ pipeline/jenkins/sub_jenkins/build_container_a.groovy
@@ -1,16 +1,16 @@
 def call() {
-    echo "【Container A】当前沙箱内的节点名: ${env.run_node_name}"
-    echo "【Container A】当前沙箱内的工作空间: ${env.WORKSPACE}"
+    echo "【Container A】控制器工作空间: ${env.WORKSPACE}"
     def ci_tool_path = "pipeline/ci_tool"
     node(env.run_node_name) {
         try {
+            echo "【Container A】当前沙箱内的节点名: ${env.run_node_name}"
+            echo "【Container A】当前运行节点: ${env.NODE_NAME}"
+            echo "【Container A】当前节点工作空间: ${env.WORKSPACE}"
             stage("【A】Prepare") {
                 deleteDir()
                 sh "git clone -q -b dev https://github.com/mmiris/client.git ."
             }
             stage("【A】Business Logic") {
-                sh "ls -lR"
-                sh "pwd"
                 // 执行 Tier 3 Python 逻辑
                 sh "python3 $ci_tool_path/binary_compare.py"
             }
--- pipeline/jenkins/sub_jenkins/build_container_b.groovy
+++ pipeline/jenkins/sub_jenkins/build_container_b.groovy
@@ -1,16 +1,16 @@
 def call() {
-    echo "【Container B】当前沙箱内的节点名: ${env.run_node_name}"
-    echo "【Container A】当前沙箱内的工作空间: ${env.WORKSPACE}"
+    echo "【Container B】控制器工作空间: ${env.WORKSPACE}"
     def ci_tool_path = "pipeline/ci_tool"
     node(env.run_node_name) {
         try {
+            echo "【Container B】当前沙箱内的节点名: ${env.run_node_name}"
+            echo "【Container B】当前运行节点: ${env.NODE_NAME}"
+            echo "【Container B】当前节点工作空间: ${env.WORKSPACE}"
             stage("【B】Prepare") {
                 deleteDir()
                 sh "git clone -q -b dev https://github.com/mmiris/client.git ."
             }
             stage("【B】Business Logic") {
-                sh "ls -lR"
-                sh "pwd"
                 // 执行 Tier 3 Python 逻辑
                 sh "python3 $ci_tool_path/binary_compare.py"
             }
